2d0075bdfba7395ff3e4c7626654dcce1d040d0a
Zamdikhanov <oxothuk.sky@mail.ru>
2022-02-05 22:39:34 +0400

    Updates

---
+++ precache-manifest.644e686b1759614a3556702b5192516d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff14b00b99af4f12e01b0ea045ef67e4",
+    "url": "/soccerstat/index.html"
+  },
+  {
+    "revision": "844b79345e2b5a729239",
+    "url": "/soccerstat/static/css/main.20dacdfc.chunk.css"
+  },
+  {
+    "revision": "dd5e30a5f64fa93fc9ba",
+    "url": "/soccerstat/static/js/2.5c92ea8c.chunk.js"
+  },
+  {
+    "revision": "a5559bb6484647199a26",
+    "url": "/soccerstat/static/js/3.2bcea6df.chunk.js"
+  },
+  {
+    "revision": "844b79345e2b5a729239",
+    "url": "/soccerstat/static/js/main.efab8018.chunk.js"
+  },
+  {
+    "revision": "9988fe7a7e75f4803943",
+    "url": "/soccerstat/static/js/runtime~main.d4ec01cb.js"
+  },
+  {
+    "revision": "1ed2d4c4fc4d950f86d5abc3eb07a9a0",
+    "url": "/soccerstat/static/media/preloader-ball.1ed2d4c4.gif"
+  }
+]);
